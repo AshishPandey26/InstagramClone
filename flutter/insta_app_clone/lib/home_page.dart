307cee1f6bb5e6de02718b3d3fde4eaf636a77ac
Ashish Pandey <pandaya10@gmail.com>
2023-12-09 20:43:11 +0530

    initial commits

---
+++ flutter/insta_app_clone/lib/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> profileImages = [
+    "images/gtr.png",
+    "images/lambo11.png",
+    "images/maclaren.png",
+    "images/supra.png",
+    "images/rr.png",
+    "images/ford.png",
+    "images/bmw.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "images/5a4e432a2da5ad73df7efe7a.png",
+          height: 50,
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline))
+        ],
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          //STORY
+
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  7,
+                  (index) => Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 25,
+                            backgroundImage: AssetImage("images/insta_bg.png"),
+                            child: CircleAvatar(
+                                radius: 21,
+                                backgroundImage:
+                                    AssetImage(profileImages[index])),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Profile Name",
+                            style:
+                                TextStyle(fontSize: 12, color: Colors.black87),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              )
+          ),
+          Divider(height: 2),
+
+          Column(
+              children: List.generate(
+                  7,
+                  (index) => Column(
+                        children: [
+                          //HEADER POST
+                          Row(children: [
+                            Container(
+                            padding: EdgeInsets.all(10),
+                            child : CircleAvatar(
+                              radius: 28,
+                                backgroundImage: AssetImage(profileImages[index]),
+                                ),
+                            )
+                         ]
+                        )
+                      ],
+                  )
+              )
+          )
+        ],
+        )
+      ),
+    );
+  }
+}
---
+++ flutter/insta_app_clone/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1)),
+        appBarTheme: AppBarTheme(
+          elevation: 1,
+         color: Colors.white,
+        iconTheme: IconThemeData(color: Color.fromRGBO(40, 40, 40, 1))),
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext Context) {
+    return Scaffold(
+      body: HomePage(),
+      bottomNavigationBar: BottomAppBar(
+          child: Row(
+            children: [
+              Spacer(),
+              IconButton(
+                  icon: Icon(Icons.home,
+                      color: currentPage == 0
+                          ? Color.fromRGBO(203, 73, 101, 4)
+                          : Color.fromRGBO(40, 40, 40, 1)),onPressed: () {
+                setState((){
+                  currentPage = 0;
+                });
+              }),
+              Spacer(),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      currentPage=1;
+                    });
+                  },
+                  icon: Icon(Icons.search,
+                      color: currentPage == 1
+                          ? Color.fromRGBO(203, 73, 101, 4)
+                          : Color.fromRGBO(40, 40, 40, 1))),
+              Spacer(),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      currentPage=1;
+                    });
+                  },
+                  icon: Icon(Icons.add_circle_outline,
+                     )),
+              Spacer(),
+              IconButton(onPressed: () {}, icon: Icon(Icons.ondemand_video)),
+              Spacer(),
+
+              IconButton(onPressed: () {}, icon: Icon(Icons.person))
+            ],
+          )),
+    );
+  }
+}
