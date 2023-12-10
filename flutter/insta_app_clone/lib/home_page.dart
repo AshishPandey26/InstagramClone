0fad0b6c196f86c6915fb2a2ac4a7f3c2a132a2c
Ashish Pandey <pandaya10@gmail.com>
2023-12-10 10:27:02 +0530

    Update home_page.dart

--- flutter/insta_app_clone/lib/home_page.dart
+++ flutter/insta_app_clone/lib/home_page.dart
@@ -33,6 +33,9 @@ class _HomePageState extends State<HomePage> {
     "ford",
     "BMW",
   ];
+  Future <void> onRefresh()async{
+    await Future.delayed(Duration(seconds: 1));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,137 +49,139 @@ class _HomePageState extends State<HomePage> {
           IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline))
         ],
       ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          //STORY
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            //STORY
 
-          SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(
-                  7,
-                  (index) => Container(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 25,
-                            backgroundImage: AssetImage("images/insta_bg.png"),
-                            child: CircleAvatar(
-                                radius: 22,
-                                backgroundImage:
-                                    AssetImage(profileImages[index])),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            nameStory[index],
-                            style:
-                                TextStyle(fontSize: 12, color: Colors.black87),
-                          )
-                        ],
-                      )),
-                ),
-              )),
-          Divider(height: 2),
-
-          Column(
-              children: List.generate(
-                  7,
-                  (index) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          //HEADER POST
-                          Row(children: [
-                            Container(
-                              padding: EdgeInsets.all(10),
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    7,
+                    (index) => Container(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundImage:
+                                  AssetImage("images/insta_bg.png"),
                               child: CircleAvatar(
-                                  radius: 19,
+                                  radius: 22,
                                   backgroundImage:
-                                      AssetImage("images/insta_bg.png"),
-                                  child: CircleAvatar(
-                                    radius: 18,
-                                    backgroundImage:
-                                        AssetImage(profileImages[index]),
-                                  )),
+                                      AssetImage(profileImages[index])),
                             ),
-                            // Text("Profile Name"),
-                            Text(nameStory[index]),
-                            Spacer(),
-                            IconButton(
-                                onPressed: () {}, icon: Icon(Icons.more_vert)),
-                          ]),
-                          //IMAGE POST
-                          Image.asset(posts[index]),
-                          //FOOTER POST
-
-                          Row(children: [
-                            IconButton(
-                              icon: Icon(Icons.favorite_border),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.comment_outlined),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.send_outlined),
-                              onPressed: () {},
-                            ),
-                            Spacer(),
-                            IconButton(
-                              icon: Icon(Icons.bookmark_border_outlined),
-                              onPressed: () {},
+                            SizedBox(height: 10),
+                            Text(
+                              nameStory[index],
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.black87),
                             )
-                          ]),
-                          Container(
-                            padding: EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                RichText(
-                                  text: TextSpan(
-                                      style: TextStyle(color: Colors.black),
-                                      children: [
-                                        TextSpan(text: "Liked by "),
-                                        TextSpan(
-                                            text: "You",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold)),
-                                        TextSpan(text: " and"),
-                                        TextSpan(
-                                            text: " others",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold))
-                                      ]),
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                      style: TextStyle(color: Colors.black),
-                                      children: [
-                                        TextSpan(
-                                            text: nameStory[index],
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold)),
-                                        TextSpan(
-                                            text:
-                                                "  This is the best of our and is One Of A Kind, and the best sounding and flaming car"),
-                                      ]
+                          ],
+                        )),
+                  ),
+                )),
+            Divider(height: 2),
+
+            Column(
+                children: List.generate(
+                    7,
+                    (index) => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //HEADER POST
+                            Row(children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: CircleAvatar(
+                                    radius: 19,
+                                    backgroundImage:
+                                        AssetImage("images/insta_bg.png"),
+                                    child: CircleAvatar(
+                                      radius: 18,
+                                      backgroundImage:
+                                          AssetImage(profileImages[index]),
+                                    )),
+                              ),
+                              // Text("Profile Name"),
+                              Text(nameStory[index]),
+                              Spacer(),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.more_vert)),
+                            ]),
+                            //IMAGE POST
+                            Image.asset(posts[index]),
+                            //FOOTER POST
+
+                            Row(children: [
+                              IconButton(
+                                icon: Icon(Icons.favorite_border),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.comment_outlined),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.send_outlined),
+                                onPressed: () {},
+                              ),
+                              Spacer(),
+                              IconButton(
+                                icon: Icon(Icons.bookmark_border_outlined),
+                                onPressed: () {},
+                              )
+                            ]),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                        style: TextStyle(color: Colors.black),
+                                        children: [
+                                          TextSpan(text: "Liked by "),
+                                          TextSpan(
+                                              text: "You",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(text: " and"),
+                                          TextSpan(
+                                              text: " others",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold))
+                                        ]),
                                   ),
-                                ),
-                                Text(
-                                  "View all 1250 comments",
-                                  style: TextStyle(
-                                    color: Colors.black45,
-                                  )
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      )))
-        ],
-      )),
+                                  RichText(
+                                    text: TextSpan(
+                                        style: TextStyle(color: Colors.black),
+                                        children: [
+                                          TextSpan(
+                                              text: nameStory[index],
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                          TextSpan(
+                                              text:
+                                                  "  This is the best of our and is One Of A Kind, and the best sounding and flaming car"),
+                                        ]),
+                                  ),
+                                  Text("View all 1250 comments",
+                                      style: TextStyle(
+                                        color: Colors.black45,
+                                      ))
+                                ],
+                              ),
+                            )
+                          ],
+                        )))
+          ],
+        )),
+      ),
     );
   }
 }
